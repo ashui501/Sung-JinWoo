0b7f4f719a354df342cca64fae2bc873b2868ecd
AKIRA ISHIKKI <113001130+ashui501@users.noreply.github.com>
2024-07-09 12:46:37 +0530

    Update alive.py

--- SungJinWoo/modules/alive.py
+++ SungJinWoo/modules/alive.py
@@ -7,7 +7,7 @@ SungJinWoo = "https://telegra.ph/file/bb2ac197bafa9619450b1.jpg"
 @register(pattern=("/alive"))
 async def awake(event):
   STB = event.sender.first_name
-  STB = "**I m SungJinWoo** \n\n" + "**I'm Working Properly**\n\n"
+  STB = "**I m Kaneki-ghoul** \n\n" + "**I'm Working Properly**\n\n"
   STB += "**Python Version : 3.9.7**\n\n"
   STB += "**python-Telegram-Bot : 13.7**\n\n"
   BUTTON = [[Button.url("Support", "https://t.me/{SUPPORT_CHAT}"), Button.url("Updates", "https://t.me/{UPDATES_CHANNEL}")]]
